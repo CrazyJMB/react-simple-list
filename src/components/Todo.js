f30f5d3e565b8b155b898f8dcd04aaa847cd5c7d
Jorge Martín Barreto <jorma28j@upv.edu.es>
2022-03-31 17:34:41 +0200

    Primer componente personalizado añadido

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo() {
+  return (
+    <div class="card">
+      <h2>TITLE</h2>
+      <div class="actions">
+        <button class="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
