a8743f05dcf57d572ce011999eafe678827fc9d8
Jorge Martín Barreto <jorma28j@upv.edu.es>
2022-03-31 18:48:48 +0200

    Modal and backdrop components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) {
+  function deleteHandler() {
+      
+  }
+
   return (
     <div class="card">
       <h2>{props.title}</h2>
       <div class="actions">
-        <button class="btn">Delete</button>
+        <button class="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
